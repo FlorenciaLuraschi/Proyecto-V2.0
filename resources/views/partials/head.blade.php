d88bfb2826c1b5376698c5eb8f57da93d6ab2199
Florencia Luraschi <tilkara17@gmail.com>
2019-07-14 22:02:04 -0300

    Agregado de vista para pagina de bienvenida

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -14,7 +14,8 @@
 <link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Merienda:400,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,44 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-      @include('partials.head')
-        {{-- <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 
-        <title>FloPaTin</title>
+<head>
+    @include('partials.head')
+    <title>FloPaTin</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/bienvenido.css">
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<body>
+    <video id="videoRtl" src="img/Teamwork.mp4" autoplay loop muted></video>
+    <div class="header-overlay"></div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <div class="flex-center position-ref full-height header-content">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Iniciar Sesión</a>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Iniciar Sesión</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Registrarse</a>
-                        @endif
-
-                        <a href="/ayuda">Ayuda</a>
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Registrarse</a>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    FloPaTin
-                </div>
-
-
-            </div>
+            <a href="/ayuda">Ayuda</a>
+            @endauth
         </div>
-        @include('partials.footer')
-    </body>
+        @endif
+
+        <div class="content">
+            <div class="title m-b-md">
+                <strong>FloPaTin</strong>
+            </div>
+            <p><strong>Somos una comunidad donde podrás jugar,
+                    un foro para socializar con otros jugadores,
+                    un blog para compartir tus experiencias de juego y también para que otros, dentro de nuestra comunidad, puedan conocerte.</strong></p>
+        </div>
+    </div>
+
+</body>
+
 </html>
